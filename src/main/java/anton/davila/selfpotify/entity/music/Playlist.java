821860c2d8da0f76d12852fba3105e6d8088248f
Anton Davila <anton.davila.carro@gmail.com>
2026-03-31 18:34:54 +0200

    feat: entidad playlist

---
+++ src/main/java/anton/davila/selfpotify/entity/music/Playlist.java
@@ -0,0 +1,37 @@
+package anton.davila.selfpotify.entity.music;
+
+import anton.davila.selfpotify.entity.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Playlist {
+
+    @Id
+    private Long id;
+
+    private List<Song> songs;
+    private int duration_ms = 0;
+    private boolean isPublic = false;
+
+    @ManyToOne
+    @JoinColumn(name = "creator_id")
+    private User creator;
+
+    // constructor mediante lista de canciones
+    public Playlist(List<Song> songs) {
+        // calcula la duracion total de las canciones
+        songs.forEach(s -> { this.duration_ms += s.getDuration_ms(); });
+        // guarda la lista de canciones en el objeto
+        this.songs = songs;
+    }
+
+}
