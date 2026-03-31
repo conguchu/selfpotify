6549fbd871148289da14d697138cd4def7e0ddbb
Anton Davila <anton.davila.carro@gmail.com>
2026-03-31 18:16:56 +0200

    fix: cambio de nombre a server, solamente será la API

--- src/main/java/anton/davila/selfpotify/SelfpotifyServer.java
+++ src/main/java/anton/davila/selfpotify/SelfpotifyServer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SelfpotifyApplication {
+public class SelfpotifyServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(SelfpotifyApplication.class, args);
+        SpringApplication.run(SelfpotifyServer.class, args);
     }
 
 }
