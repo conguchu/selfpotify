e9218a7df60b29a42717304ed9087a0594e022d4
Anton Davila <anton.davila.carro@gmail.com>
2026-03-31 18:10:33 +0200

    first commits

---
+++ src/main/java/anton/davila/selfpotify/SelfpotifyApplication.java
@@ -0,0 +1,13 @@
+package anton.davila.selfpotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfpotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfpotifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/anton/davila/selfpotify/SelfpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package anton.davila.selfpotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfpotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
