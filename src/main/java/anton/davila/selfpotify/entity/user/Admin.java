7d121ba4278b9a702f065baf24c06e70a85389f2
Anton Davila <anton.davila.carro@gmail.com>
2026-04-06 15:08:11 +0200

    fix: discriminator value admin

--- src/main/java/anton/davila/selfpotify/entity/user/Admin.java
+++ src/main/java/anton/davila/selfpotify/entity/user/Admin.java
@@ -1,10 +1,11 @@
 package anton.davila.selfpotify.entity.user;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Data;
 
 @Entity
 @Data
+@DiscriminatorValue("ADMIN")
 public class Admin extends User {
-    // single table -> columna type (admin o user)
 }
