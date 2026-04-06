23427218e276ca6fec22ffa3320b221eeddcf01a
Anton Davila <anton.davila.carro@gmail.com>
2026-04-06 12:52:42 +0200

    fix: import mal

--- src/main/java/anton/davila/selfpotify/entity/music/Playlist.java
+++ src/main/java/anton/davila/selfpotify/entity/music/Playlist.java
@@ -1,10 +1,7 @@
 package anton.davila.selfpotify.entity.music;
 
 import anton.davila.selfpotify.entity.user.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
