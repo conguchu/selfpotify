7595bd8129dbcef225646a982c8508f30433cba2
Anton Davila <anton.davila.carro@gmail.com>
2026-04-06 15:08:21 +0200

    fix: discriminator value admin (single table)

--- src/main/java/anton/davila/selfpotify/entity/user/User.java
+++ src/main/java/anton/davila/selfpotify/entity/user/User.java
@@ -1,14 +1,14 @@
 package anton.davila.selfpotify.entity.user;
 
 import anton.davila.selfpotify.entity.user.profile.Profile;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type")
+@DiscriminatorValue("USER")
 public class User {
     @Id
     private Long id;
