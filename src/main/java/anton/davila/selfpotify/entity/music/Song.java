83cb57ae82202014d7953a5e128ccdb456222418
Anton Davila <anton.davila.carro@gmail.com>
2026-04-07 09:36:29 +0200

    fix: constructors

--- src/main/java/anton/davila/selfpotify/entity/music/Song.java
+++ src/main/java/anton/davila/selfpotify/entity/music/Song.java
@@ -1,12 +1,16 @@
 package anton.davila.selfpotify.entity.music;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Song {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
