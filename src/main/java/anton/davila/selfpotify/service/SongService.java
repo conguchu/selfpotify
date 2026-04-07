76b5376349b7789db8aa595397d125377be13971
Anton Davila <anton.davila.carro@gmail.com>
2026-04-07 18:05:09 +0200

    feat:crud a medias

--- src/main/java/anton/davila/selfpotify/service/SongService.java
+++ src/main/java/anton/davila/selfpotify/service/SongService.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SongService {
@@ -16,14 +17,29 @@ public class SongService {
     private SongRepository songRepository;
 
 
+    // =====================================
     // ----- CRUD
+    // =====================================
     public Song add(Song s) {
         return songRepository.save(s);
     }
-
     public List<Song> getAll() {
         return songRepository.findAll();
     }
+    public Optional<Song> getById(long id) {
+        return songRepository.findById(id);
+    }
+    public Song update(long id, Song song) {
+        Optional<Song> old = getById(id);
+        if (old.isEmpty()) {
+            throw new RuntimeException("No se ha encontrado la cancion con ID " + id);
+        }
+        return null;
+        // todo: song.copy(old)
+        // (metodo para copiar los atributos del objeto sin tener que hacerlo a mano siempre)
+    }
+
+
 
 
 }
