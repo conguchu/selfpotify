950c00ae98ae92aebc2edfe6b3740b22693e01bf
Anton Davila <anton.davila.carro@gmail.com>
2026-04-22 14:13:21 +0200

    feat: playlist publicas y privadas

--- src/main/java/anton/davila/selfpotify/music/repository/PlaylistRepository.java
+++ src/main/java/anton/davila/selfpotify/music/repository/PlaylistRepository.java
@@ -1,7 +1,13 @@
 package anton.davila.selfpotify.music.repository;
 
 import anton.davila.selfpotify.music.entity.Playlist;
+import anton.davila.selfpotify.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+    List<Playlist> findByCreator(User creator);
+    List<Playlist> findByCreatorAndIsPublicTrue(User creator);
+    List<Playlist> findByIsPublicTrue();
 }
--- src/main/java/anton/davila/selfpotify/music/service/PlaylistService.java
+++ src/main/java/anton/davila/selfpotify/music/service/PlaylistService.java
@@ -27,6 +27,21 @@ public class PlaylistService {
         return playlistRepository.findAll();
     }
 
+    public List<Playlist> getByUser(anton.davila.selfpotify.user.entity.User user) {
+        log.info("Recuperando playlists del usuario: {}", user.getUsername());
+        return playlistRepository.findByCreator(user);
+    }
+
+    public List<Playlist> getPublicByUser(anton.davila.selfpotify.user.entity.User user) {
+        log.info("Recuperando playlists públicas del usuario: {}", user.getUsername());
+        return playlistRepository.findByCreatorAndIsPublicTrue(user);
+    }
+
+    public List<Playlist> getAllPublic() {
+        log.info("Recuperando todas las playlists públicas");
+        return playlistRepository.findByIsPublicTrue();
+    }
+
     public Optional<Playlist> getById(long id) {
         log.info("Buscando playlist por ID: {}", id);
         return playlistRepository.findById(id);
