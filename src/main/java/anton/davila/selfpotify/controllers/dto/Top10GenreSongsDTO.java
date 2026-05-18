5681d02e7a7062c8182204844f921a27e352e47f
Anton Davila <anton.davila.carro@gmail.com>
2026-05-18 17:29:22 +0200

    (feat) @Data

--- src/main/java/anton/davila/selfpotify/controllers/dto/Top10GenreSongsDTO.java
+++ src/main/java/anton/davila/selfpotify/controllers/dto/Top10GenreSongsDTO.java
@@ -1,9 +1,11 @@
 package anton.davila.selfpotify.controllers.dto;
 
 import anton.davila.selfpotify.music.entity.Song;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class Top10GenreSongsDTO {
     private String genre;
     private List<Song> top;
