23f6c34c97fa08e4c9c940a2916c4f9686a26621
Anton Davila <anton.davila.carro@gmail.com>
2026-05-18 02:45:45 +0200

    (feat) nueva query JPQL findTop10ByArtistIdOrderByListenersDesc que hace join con la tabla song_artist y ordena por listeners desc limitado a 10.

--- src/main/java/anton/davila/selfpotify/music/repository/SongRepository.java
+++ src/main/java/anton/davila/selfpotify/music/repository/SongRepository.java
@@ -6,12 +6,17 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 import java.util.Optional;
 
 public interface SongRepository extends JpaRepository<Song, Long> {
     @Modifying
     @Query("update Song s set s.listeners = s.listeners + 1 where s.id = :id")
     void incrementListeners(@Param("id") Long id);
+
+    @Query("select s from Song s join s.artists a where a.id = :artistId order by s.listeners desc limit 10")
+    List<Song> findTop10ByArtistIdOrderByListenersDesc(@Param("artistId") Long artistId);
 
     Optional<Song> findFirstBySongPath(String songPath);
 }
