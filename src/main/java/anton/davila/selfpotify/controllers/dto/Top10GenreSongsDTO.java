e76cea33520d645668773802776be31d91fa8ba4
Anton Davila <anton.davila.carro@gmail.com>
2026-05-18 17:36:19 +0200

    (feat) constructores con y sin args

--- src/main/java/anton/davila/selfpotify/controllers/dto/Top10GenreSongsDTO.java
+++ src/main/java/anton/davila/selfpotify/controllers/dto/Top10GenreSongsDTO.java
@@ -1,11 +1,15 @@
 package anton.davila.selfpotify.controllers.dto;
 
 import anton.davila.selfpotify.music.entity.Song;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Top10GenreSongsDTO {
     private String genre;
     private List<Song> top;
